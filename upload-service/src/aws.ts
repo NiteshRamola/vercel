5ee3aaa8b95b084e85e223e2c9eee8934601f18b
Nitesh Ramola <niteshramola90@gmail.com>
2024-02-24 20:05:56 +0530

    added error handling & changed to async

--- upload-service/src/aws.ts
+++ upload-service/src/aws.ts
@@ -1,10 +1,10 @@
 import { S3 } from 'aws-sdk';
-import fs from 'fs';
+import fs from 'fs/promises';
 
 const s3 = new S3({});
 
 export const uploadFile = async (fileName: string, localFilePath: string) => {
-  const fileContent = fs.readFileSync(localFilePath);
+  const fileContent = await fs.readFile(localFilePath);
   const response = await s3
     .upload({
       Body: fileContent,
--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -17,41 +17,53 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/deploy', async (req: Request, res: Response) => {
-  const repoUrl: string = req.body.repoUrl;
+  try {
+    const repoUrl: string = req.body.repoUrl;
 
-  if (!repoUrl) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'Repo url is required' });
+    if (!repoUrl) {
+      return res
+        .status(400)
+        .json({ success: false, msg: 'Repo url is required' });
+    }
+
+    const id = generateId();
+    const outputDir = path.join(__dirname, `../clonedRepos/${id}`);
+
+    await simpleGit().clone(repoUrl, outputDir);
+
+    const files = await getAllFiles(outputDir);
+    for await (let file of files) {
+      await uploadFile(file.split('vercel/')[1], file);
+    }
+
+    redisPubSub.lPush('build-queue', id);
+    redisPubSub.hSet('status', id, 'uploaded');
+
+    await removeAllFiles(outputDir);
+
+    res.json({ success: true, id });
+  } catch (error) {
+    console.error('Error during uploading:', error);
+    res.status(500).json({ success: false, msg: 'Internal server error' });
   }
-
-  const id = generateId();
-  const outputDir = path.join(__dirname, `../clonedRepos/${id}`);
-
-  await simpleGit().clone(repoUrl, outputDir);
-
-  const files = await getAllFiles(outputDir);
-  for await (let file of files) {
-    await uploadFile(file.split('vercel/')[1], file);
-  }
-
-  await await new Promise((resolve) => setTimeout(resolve, 5000));
-  redisPubSub.lPush('build-queue', id);
-
-  redisPubSub.hSet('status', id, 'uploaded');
-
-  await removeAllFiles(outputDir);
-
-  res.json({ success: true, id });
 });
 
 app.get('/status', async (req, res) => {
-  const id = req.query.id;
-  const response = await redisPubSub.hGet('status', id as string);
+  try {
+    const id = req.query.id;
 
-  res.json({
-    status: response,
-  });
+    if (!id) {
+      return res.status(400).json({ success: false, msg: 'ID is required' });
+    }
+
+    const response = await redisPubSub.hGet('status', id as string);
+    res.json({
+      status: response,
+    });
+  } catch (error) {
+    console.error('Error fetching status:', error);
+    res.status(500).json({ success: false, msg: 'Internal server error' });
+  }
 });
 
 app.listen(3000, () => {
