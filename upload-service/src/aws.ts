5e7ffb152f81f75df2d17ee895c8539d6f474185
Nitesh Ramola <niteshramola90@gmail.com>
2024-02-26 16:25:02 +0530

    added env variables

--- upload-service/src/aws.ts
+++ upload-service/src/aws.ts
@@ -1,7 +1,11 @@
 import { S3 } from 'aws-sdk';
 import fs from 'fs/promises';
 
-const s3 = new S3({});
+const s3 = new S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
 export const uploadFile = async (fileName: string, localFilePath: string) => {
   const fileContent = await fs.readFile(localFilePath);
