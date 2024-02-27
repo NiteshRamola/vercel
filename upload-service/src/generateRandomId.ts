44b6dd07883c31e823c76ab60934add504581877
Nitesh Ramola <niteshramola90@gmail.com>
2024-02-27 10:48:11 +0530

    cleanup for version 2

--- upload-service/src/generateRandomId.ts
+++
@@ -1,9 +0,0 @@
-const MAX_LEN = 5;
-const SUBSET = '1234567890qwertyuiopasdfghjklzxcvbnm';
-const SUBSET_LENGTH = SUBSET.length;
-
-export const generateId = (): string => {
-  return Array.from({ length: MAX_LEN }, () =>
-    SUBSET.charAt(Math.floor(Math.random() * SUBSET_LENGTH)),
-  ).join('');
-};
